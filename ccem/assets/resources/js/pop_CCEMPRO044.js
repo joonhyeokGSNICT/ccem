f1a998f775eb20107a651aa4f99e7b4cd7740b1b
joonhyeokc <joonhyeokcc@gmail.com>
2020-12-14 17:53:38 +0900

    사업국 트리구조 TEST

--- ccem/assets/resources/js/pop_CCEMPRO044.js
+++ ccem/assets/resources/js/pop_CCEMPRO044.js
@@ -1,7 +1,36 @@
 var recordGrid;
+var tree;
 
 $(function(){
-	$("#tree").fancytree();
+	$("#tree").fancytree({
+				click: function(event, data) {
+					console.log(event, data, ", targetType=" + data.targetType);
+
+					if(data.node.parent != null){
+						if(data.node.type == "지점"){
+							//alert("dd");
+							$("#HQ_NAME").text(data.node.parent.title);
+							$("#DEPT_NAME").text(data.node.title);
+							//$("#LC_NAME").text(data.node.title);
+							$("#POSTNUM").val(data.node.data.POSTNUM);
+							$("#POSTADDR").val(data.node.data.POSTADDR);
+							$("#ADDR").val(data.node.data.ADDR);
+							$("#PHONE").val(data.node.data.PHONE);
+							$("#FAXNUM").val(data.node.data.FAXNUM);
+							
+						}else if(data.node.type == "센터"){
+							$("#HQ_NAME").text(data.node.parent.parent.title);
+							$("#DEPT_NAME").text(data.node.parent.title);
+							$("#LC_NAME").text(data.node.title);
+							$("#POSTNUM").val(data.node.data.POSTNUM);
+							$("#POSTADDR").val(data.node.data.POSTADDR);
+							$("#ADDR").val(data.node.data.ADDR);
+							$("#PHONE").val(data.node.data.PHONE);
+							$("#FAXNUM").val(data.node.data.FAXNUM);
+						}
+					}
+				}
+			});
 	
 	$("input[name=searchType]").change(function(){
 		var selectedSearch = $(this).attr("id");
@@ -14,7 +43,75 @@ $(function(){
 		}
 	});
 	
+	tree = $.ui.fancytree.getTree("#tree");
 	
+	tree.reload([
+        {title: "북서울BCG", folder:true, expanded:true, children:[
+        	{title: "용산HL", type:"지점", POSTNUM:"04317",POSTADDR:"서울 용산구 효창동",ADDR:"5-86번지 대정빌딩 2층", PHONE:"02-718-2295", FAXNUM:"02-711-2294"},
+        	{title: "마포HL"},
+        	{title: "은평HL"},
+        	{title: "덕양HL"},
+        	{title: "일산HL"},
+        	{title: "교하HL"},
+        	{title: "파주HL"},
+        	{title: "도봉HL"},
+        	{title: "성북HL"},
+        	{title: "노원HL"},
+        	{title: "동대문HL"},
+        	{title: "성동광진HL"},
+        	{title: "최덕규BCL", folder:true, children:[
+        		{title: "갈현", type:"센터", POSTNUM:"04317",POSTADDR:"경기 고양시 일산동구 탄중로 323",ADDR:"86-31번지 미주빌딩", PHONE:"02-384-9509", FAXNUM:""},
+        		{title: "홍은"},
+        		{title: "별빛드림"},
+        		{title: "중산중앙"},
+        		{title: "장위"},
+        		{title: "한신한진"},
+        		{title: "무학"},
+        		{title: "길음"},
+        		{title: "정릉"},
+        		{title: "공릉"},
+        	]},
+        	{title: "홍기연BCL", folder:true, children:[
+        		{title: "갈현", type:"센터", POSTNUM:"04317",POSTADDR:"경기 고양시 일산동구 탄중로 323",ADDR:"86-31번지 미주빌딩"},
+        		{title: "홍은"},
+        		{title: "별빛드림"},
+        		{title: "중산중앙"},
+        		{title: "장위"},
+        		{title: "한신한진"},
+        		{title: "무학"},
+        		{title: "길음"},
+        		{title: "정릉"},
+        		{title: "공릉"},
+        	]},
+        	{title: "황영애BCL", folder:true, children:[
+        		{title: "갈현"},
+        		{title: "홍은"},
+        		{title: "별빛드림"},
+        		{title: "중산중앙"},
+        		{title: "장위"},
+        		{title: "한신한진"},
+        		{title: "무학"},
+        		{title: "길음"},
+        		{title: "정릉"},
+        		{title: "공릉"},
+        	]},
+        	{title: "이은희BCL", folder:true, children:[
+        		{title: "갈현"},
+        		{title: "홍은"},
+        		{title: "별빛드림"},
+        		{title: "중산중앙"},
+        		{title: "장위"},
+        		{title: "한신한진"},
+        		{title: "무학"},
+        		{title: "길음"},
+        		{title: "정릉"},
+        		{title: "공릉"},
+        	]}
+        ]},
+        {title: "node2"}
+      ]).done(function(){
+        //alert("reloaded");
+      });
 	
 	// 녹취LIST
 	recordGrid = new Grid({
