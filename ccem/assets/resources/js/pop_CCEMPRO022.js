044c5938e100cbad58a227aff72f8f23fe8303d2
chiwonoh <chiwon@gsnict.com>
2020-12-18 18:03:27 +0900

    상담등록 과목 조회, 선택, 검색 로직 추가

--- ccem/assets/resources/js/pop_CCEMPRO022.js
+++ ccem/assets/resources/js/pop_CCEMPRO022.js
@@ -1,4 +1,5 @@
 let grid1, grid2, grid3, grid4, grid5;
+let prodList;	// 과목리스트
 
 $(function () {
 
@@ -14,15 +15,25 @@ $(function () {
 	// grid refreshLayout
 	$('.nav-link').on('shown.bs.tab', ev => refreshGrid(ev.target.id));
 
-	// input mask
-	$(".imask-date").each((i, el) => calendarUtil.init(el.id));
-	$(".imask-date-up").each((i, el) => calendarUtil.init(el.id, { drops: "up" }));
-	$(".imask-time").each((i, el) => calendarUtil.timeMask(el.id));
+	// create calendar
+	$(".calendar").each((i, el) => calendarUtil.init(el.id));
+	calendarUtil.init("calendar2", { drops: "up" });
 	calendarUtil.init("calendar5", { opens: "center" });
+
+	// input mask
+	$(".imask-date").each((i, el) => calendarUtil.dateMask(el.id));
+	$(".imask-time").each((i, el) => calendarUtil.timeMask(el.id));
+
+	// 날짜와 시간 초기값 세팅
+	$("#calendar1").val(getDateFormat());
+	$("#calendar2").val(getDateFormat());
+	$(".imask-date-up").val(getDateFormat());
+	$("#time1").val(getTimeFormat());
 
 	createGrids();
 	getCodeList()
 	getProd();
+
 
 });
 
@@ -73,6 +84,12 @@ const createGrids = () => {
 		grid1.clickSort(ev);
 		grid1.clickCheck(ev);
 	});
+	grid1.on("check", ev => {
+		checkProd(grid1, grid2);
+	});
+	grid1.on("uncheck", ev => {
+		checkProd(grid1, grid2);
+	});
 
 	// 상담등록 > 상담과목 grid
 	grid2 = new Grid({
@@ -89,8 +106,8 @@ const createGrids = () => {
 		columns: [
 			{
 				header: '상담과목',
-				name: 'name1',
-				align: "center",
+				name: 'PRDT_NAME',
+				align: "left",
 				sortable: true,
 				ellipsis: true,
 			}
@@ -184,6 +201,12 @@ const createGrids = () => {
 		grid4.clickSort(ev);
 		grid4.clickCheck(ev);
 	});
+	grid4.on("check", ev => {
+		checkProd(grid4, grid5);
+	});
+	grid4.on("uncheck", ev => {
+		checkProd(grid4, grid5);
+	});
 
 	// 입회등록 > 입회과목 grid
 	grid5 = new Grid({
@@ -200,8 +223,8 @@ const createGrids = () => {
 		columns: [
 			{
 				header: '입회과목',
-				name: 'name1',
-				align: "center",
+				name: 'PRDT_NAME',
+				align: "left",
 				sortable: true,
 				ellipsis: true,
 			}
@@ -267,10 +290,10 @@ const openPopup = (key) => {
 			else
 				PopupUtil.open(key, 1098, 810);
 			break;
-		case "CCEMPRO033":
+		case "CCEMPRO033":	// 고객조회
 			PopupUtil.open(key, 1184, 650);
 			break;
-		case "CCEMPRO034":
+		case "CCEMPRO034":	// 선생님조회
 			PopupUtil.open("CCEMPRO033", 1184, 650, "#counselMain_teacherSearchTab");
 			break;
 		case "CCEMPRO042": // 분류
@@ -288,8 +311,112 @@ const openPopup = (key) => {
  * @param {string} keyword
  */
 const searchProd = (grid, column, keyword) => {
-	grid.unfilter();
-	grid.filter(column, [{ code: 'contain', value: keyword }]);
+
+	// grid.unfilter();
+	// grid.filter(column, [{ code: 'contain', value: keyword }]);
+
+	// filter
+	keyword = keyword.toUpperCase();
+	let data = prodList.filter(el => (el[column].toUpperCase()).includes(keyword));
+
+	// delete rowKey
+	data = data.map(el => {
+		return {
+			PRDT_CDE: 	el.PRDT_CDE,
+			PRDT_GRP: 	el.PRDT_GRP,
+			PRDT_GRPNM: el.PRDT_GRPNM,
+			PRDT_ID: 	el.PRDT_ID,
+			PRDT_NAME: 	el.PRDT_NAME,
+			PRDT_SEQ: 	el.PRDT_SEQ,
+			USE: 		el.USE,
+			USE_FLAG: 	el.USE_FLAG
+		};
+	});
+
+	grid.resetData(data);
+
+}
+
+/**
+ * 과목선택
+ * @param {object} fromGrid grid
+ * @param {object} toGrid grid
+ */
+const checkProd = (fromGrid, toGrid) => {
+
+	// get checked rows
+	let data = fromGrid.getCheckedRows();
+
+	// delete rowKey
+	data.map(el => {
+		return {
+			PRDT_CDE: 	el.PRDT_CDE,
+			PRDT_GRP: 	el.PRDT_GRP,
+			PRDT_GRPNM: el.PRDT_GRPNM,
+			PRDT_ID: 	el.PRDT_ID,
+			PRDT_NAME: 	el.PRDT_NAME,
+			PRDT_SEQ: 	el.PRDT_SEQ,
+			USE: 		el.USE,
+			USE_FLAG: 	el.USE_FLAG
+		};
+	});
+
+	toGrid.resetData(data);
+
+}
+
+/**
+ * api 오류 체크
+ * @param {object} response 
+ * @param {object} settings 
+ */
+const checkApi = (response, settings) => {
+	if (response.errcode != "0") {
+		if(typeof client != 'undefined') {
+			let errMsg = `서버에서 오류가 발생하였습니다.<br><br>오류코드 : ${response.errcode}<br>오류메시지 : ${response.errmsg}<br>호출서비스 : ${settings.url}`;
+			client.invoke("notify", errMsg, "error", 60000);
+		}
+		else {
+			let errMsg = `서버에서 오류가 발생하였습니다.\n\n오류코드 : ${response.errcode}\n오류메시지 : ${response.errmsg}\n호출서비스 : ${settings.url}`
+			alert(errMsg);
+		}
+		return false;
+	}
+	return true;
+}
+
+/**
+ * 날짜계산 함수
+ * @param {string} type year, month, day
+ * @param {number} num 
+ */
+const getDateFormat = (type, num) => {
+    let date = new Date();
+    if (typeof type == "string" && typeof num == "number") {
+        if (type == "year") {
+            date.setFullYear(date.getFullYear() + num);
+        } else if (type == "month") {
+            date.setMonth(date.getMonth() + num);
+        } else if (type == "day") {
+            date.setDate(date.getDate() + num);
+        }
+    }
+    let y = date.getFullYear();
+    let m = ("0" + (date.getMonth() + 1)).slice(-2);
+    let d = ("0" + date.getDate()).slice(-2);
+    return y + "-" + m + "-" + d;
+}
+
+/**
+ * 
+ */
+const getTimeFormat = () => {
+	let today = new Date();   
+	let hours = today.getHours(); 
+	let minutes = today.getMinutes();
+	let seconds = today.getSeconds();
+	let milliseconds = today.getMilliseconds();
+	return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -297,10 +424,10 @@ const searchProd = (grid, column, keyword) => {
  */
 const getCodeList = () => {
 
-	let CODE_MK_LIST = ["PRDT_GRP", ];	// 과목군
+	let CODE_MK_LIST = ["PRDT_GRP",];	// 과목군
 
 	CODE_MK_LIST.forEach(codeName => {
-		$.ajax({
+		let settings = {
 			url: `${API_SERVER}/sys.getCommCode.do`,
 			method: 'POST',
 			contentType: "application/json; charset=UTF-8",
@@ -310,24 +437,24 @@ const getCodeList = () => {
 				recvdataids: ["dsRecv"],
 				dsSend: [{ CODE_MK: codeName }],
 			}),
-			success: (data) => {
-				if(data.errcode == "0") {
-					let codeList = data.dsRecv;
-					codeList.forEach(code => {
-						$(`select[name='${codeName}']`).append(new Option(code.CODE_NAME, code.CODE_ID));
-					});
-				}
-			},
+		}
+		$.ajax(settings).done(data => {
+			if (checkApi(data, settings)) {
+				let codeList = data.dsRecv;
+				codeList.forEach(code => {
+					$(`select[name='${codeName}']`).append(new Option(code.CODE_NAME, code.CODE_ID));
+				});
+			}
 		});
 	});
-	
+
 }
 
 /**
  * 상담 과목 리스트 조회
  */
 const getProd = () => {
-	$.ajax({
+	let settings = {
 		url: `${API_SERVER}/cns.getProd.do`,
 		method: 'POST',
 		contentType: "application/json; charset=UTF-8",
@@ -337,14 +464,13 @@ const getProd = () => {
 			recvdataids: ["dsRecv"],
 			dsSend: [{}],
 		}),
-		success: (data) => {
-			if (data.errcode != "0") {
-				alert(`[API ERROR] ${data.errcode} ${data.errmsg}`);
-				return;
-			}
+	}
+	$.ajax(settings).done((data) => {
+		if (checkApi(data, settings)) {
+			prodList = data.dsRecv;
 			grid1.resetData(data.dsRecv);
 			grid4.resetData(data.dsRecv);
-		},
+		}
 	});
 }
 
