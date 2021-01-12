84f282460bb18e4a443ace88b6841aa4bd0d8bcb
chiwonoh <gbajs2018@gmail.com>
2021-01-12 13:42:58 +0900

    [티켓사이드바] 티켓오픈시 탑바로 사이드바 Guid 전달

--- ccem/assets/resources/js/ticket_test.js
+++ ccem/assets/resources/js/ticket_test.js
@@ -1,22 +1,13 @@
-var sideBarClient;
 var topBarClient;
 
-//topbar is Ready
-client.on("topBarisReady", function(sideBarClient_d) {
-	sideBarClient = sideBarClient_d;
-});
-
 $(function(){
-	sidebarClient = client;
-	console.log(sideBarClient);
 	
 	client.get('instances').then(function(instancesData) {
 		var instances = instancesData.instances;
-		console.log('client instances : ', instances);
 		for ( var instanceGuid in instances) {
 			if (instances[instanceGuid].location === 'top_bar') {
-				//console.log('topbar instanceGuid : ', instanceGuid);
 				topBarClient =  client.instance(instanceGuid);
+				topBarClient.trigger("getSidebarClient", client._instanceGuid);
 			}
 		}
 	});
