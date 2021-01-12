baba6168f9d05dca0ad80c3d1fd34b99a4dfcd33
chiwonoh <gbajs2018@gmail.com>
2021-01-12 09:38:53 +0900

    [관계회원 팝업] 관계회원조회 및 기능구현

--- ccem/assets/resources/js/pop_CCEMPRO024.js
+++ ccem/assets/resources/js/pop_CCEMPRO024.js
@@ -1,77 +1,41 @@
 let grid1, grid2;
 
-$(function() {
+$(function () {
 
 	// create grid
-    grid1 = new Grid({
+	grid1 = new Grid({
 		el: document.getElementById('grid1'),
 		bodyHeight: 100,
 		scrollX: false,
 		rowHeaders: [
-			{
-				type: 'rowNum',
-				header: "NO",
-			},
+			{ type: 'rowNum', header: "NO", },
 		],
 		columns: [
-			{
-				header: '회원명',
-				name: 'name1',
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-			},
-			{
-				header: '회원번호',
-				name: 'name2',
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-			},
-			{
-				header: '전화번호',
-				name: 'name3',
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-			},
+			{ header: '회원명', 	name: 'name1', align: "center", sortable: true, ellipsis: true, },
+			{ header: '회원번호', 	name: 'name2', align: "center", sortable: true, ellipsis: true, },
+			{ header: '전화번호', 	name: 'name3', align: "center", sortable: true, ellipsis: true, },
 		],
 	});
 	grid1.on("click", (ev) => {
 		grid1.addSelection(ev);
 		grid1.clickSort(ev);
-    });
+	});
 
-    grid2 = new Grid({
+	grid2 = new Grid({
 		el: document.getElementById('grid2'),
 		bodyHeight: 288,
 		scrollX: false,
 		rowHeaders: [
-			{
-				type: 'rowNum',
-				header: "NO",
-			},
+			{ type: 'rowNum', header: "NO", },
 		],
 		columns: [
-			{
-				header: '과목명',
-				name: 'name1',
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-			},
-			{
-				header: '학습횟수',
-				name: 'name2',
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-			},
+			{ header: '과목명', 	name: 'name1', align: "center", sortable: true, ellipsis: true, },
+			{ header: '학습횟수', 	name: 'name2', align: "center", sortable: true, ellipsis: true, },
 		],
 	});
 	grid2.on("click", (ev) => {
 		grid2.addSelection(ev);
 		grid2.clickSort(ev);
-    });
-    
+	});
+
 });
--- ccem/assets/resources/js/pop_CCEMPRO029.js
+++ ccem/assets/resources/js/pop_CCEMPRO029.js
@@ -1,88 +1,90 @@
 let grid;
 
-$(function() {
+$(function () {
+    createGrids();
+    onStart(opener ? opener.name : "");
+});
+
+const createGrids = () => {
     grid = new Grid({
         el: document.getElementById("grid"),
         bodyHeight: 300,
         scrollX: false,
         rowHeaders: [
-			{
-				type: 'rowNum',
-				header: "NO",
-				minWidth: 30,
-            },
+            { type: 'rowNum', header: "NO", minWidth: 30, },
         ],
         columns: [
-			{
-				header: '구분',
-                name: 'name1',
-                width: 50,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '관계',
-                name: 'name2',
-                width: 50,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '회원명',
-                name: 'name3',
-                width: 100,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '학년',
-                name: 'name4',
-                width: 100,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '성별',
-                name: 'name5',
-                width: 50,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '회원번호',
-                name: 'name6',
-                width: 150,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
-			{
-				header: '고객번호',
-                name: 'name7',
-                minWidth: 150,
-				align: "center",
-				sortable: true,
-				ellipsis: true,
-            },
+            { header: "구분", name: "CNT_WHERE", width: 50, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "관계", name: "FAT_REL_NAME", width: 50, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "회원명", name: "FML_NAME", width: 100, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "학년", name: "GRADE_NAME", width: 100, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "성별", name: "GND_NAME", width: 50, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "회원번호", name: "MBR_ID", width: 150, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "고객번호", name: "CUST_ID", width: 150, align: "center", sortable: true, ellipsis: true, hidden: false, },
+            { header: "세대주주민번호", name: "FAT_RSDNO", width: 50, align: "center", sortable: true, ellipsis: true, hidden: true, },
         ],
         summary: {
             height: 28,
             position: 'bottom',
             columnContent: {
                 name1: "Total",
-                name2: {
-                    template: valueMap => valueMap.cnt
-                }
+                name2: { template: valueMap => valueMap.cnt },
             },
         },
     });
+
     grid.on("click", (ev) => {
         grid.addSelection(ev);
         grid.clickSort(ev);
     });
-});
+
+    grid.on("dblclick", (ev) => {
+
+        const openerName = opener ? opener.name : "";
+
+        // 상담등록 > 관계회원 버튼으로 오픈했을 때.
+        if (openerName.includes("CCEMPRO022")) {
+            const rowData = grid.getRow(ev.rowKey);
+            opener.addCselByFamily(rowData);
+            window.close();
+        }
+
+    });
+}
+
+/**
+ * 오픈하는 곳에 따라 분기처리.
+ * @param {string} openerName 
+ */
+const onStart = (openerName) => {
+
+    // 상담등록 > 관계회원 버튼으로 오픈했을 때.
+    if (openerName.includes("CCEMPRO022")) {
+        const custId = opener.document.getElementById("hiddenbox6").value;
+        getFamily(custId);
+    }
+
+}
+
+/**
+ * 관계회원 조회
+ * @param {string} custId 고객번호
+ */
+const getFamily = (custId) => {
+    const settings = {
+		url: `${API_SERVER}/cns.getFamilyInfo.do`,
+		method: 'POST',
+		contentType: "application/json; charset=UTF-8",
+		dataType: "json",
+		data: JSON.stringify({
+			senddataids: ["dsSend"],
+			recvdataids: ["dsRecv"],
+			dsSend: [{ CUST_ID: custId}],
+        }),
+        errMsg: "관계회원 조회중 오류가 발생하였습니다.",
+	}
+	$.ajax(settings).done(data => {
+        if (!checkApi(data, settings)) return;
+        grid.resetData(data.dsRecv);
+	});
+}
