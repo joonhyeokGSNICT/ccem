721277bb43ffe2f6ad08722aef626b4646adf36f
chiwonoh <gbajs2018@gmail.com>
2021-01-15 16:07:59 +0900

    [background] add topbar preloadPane

--- ccem/assets/resources/js/background.js
+++ ccem/assets/resources/js/background.js
@@ -1,46 +1,46 @@
-var sideBarClient;
-var topBarClient;
-
 var codeData;
 
-//topbar is Ready
-client.on("topBarisReady", function(sideBarClient_d) {
-	sideBarClient = sideBarClient_d;
+$(function () {
+	getCommCode();
+	tobbarPreloadPane();
 });
 
-client.on("getCodeList", function(topBarClient_d) {
-	//topBarClient = topBarClient_d;
-	topBarClient = client.instance(topBarClient_d);
-	//console.log(topBarClient);
-	topBarClient.trigger('getCodeData', codeData);
+client.on("getCodeList", function (target_guid) {
+	const target = client.instance(target_guid);
+	target.trigger('getCodeData', codeData);
 });
 
-$(function(){
-	/*sidebarClient = client;
-	console.log(sideBarClient);
-	
-	client.get('instances').then(function(instancesData) {
-		var instances = instancesData.instances;
-		console.log('client instances : ', instances);
-		for ( var instanceGuid in instances) {
-			if (instances[instanceGuid].location === 'top_bar') {
-				//console.log('topbar instanceGuid : ', instanceGuid);
-				topBarClient =  client.instance(instanceGuid);
-			}
-		}
-	});*/
+/**
+ * 공통코드 조회
+ */
+const getCommCode = () => {
 	let settings = {
-			url: `${API_SERVER}/sys.getCommCode.do`,
-			method: 'POST',
-			contentType: "application/json; charset=UTF-8",
-			dataType: "json",
-			data: JSON.stringify({
-				senddataids: ["dsSend"],
-				recvdataids: ["dsRecv"],
-				dsSend: [{ }],
-			}),
-		}
+		url: `${API_SERVER}/sys.getCommCode.do`,
+		method: 'POST',
+		contentType: "application/json; charset=UTF-8",
+		dataType: "json",
+		data: JSON.stringify({
+			senddataids: ["dsSend"],
+			recvdataids: ["dsRecv"],
+			dsSend: [{}],
+		}),
+	}
 	$.ajax(settings).done(data => {
 		codeData = data.dsRecv;
 	});
-});
+}
+
+/**
+ * 탑바를 미리로드함.
+ */
+const tobbarPreloadPane = async () => {
+	const { instances } = await client.get('instances');
+	for (const instanceGuid in instances) {
+		if (instances[instanceGuid].location === 'top_bar') {
+			const topbarClient = client.instance(instanceGuid);
+			await topbarClient.invoke('preloadPane');
+			console.debug("topbar preloadPane success!");
+			break;
+		}
+	}
+}
