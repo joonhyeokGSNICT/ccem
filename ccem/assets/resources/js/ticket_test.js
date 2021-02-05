e6a64f732ef5d2b5eb5ac82156d04eb748020148
chiwonoh <gbajs2018@gmail.com>
2021-02-05 10:37:38 +0900

    [ticket_sidebar] getSidebarClient 호출속도 개선

--- ccem/assets/resources/js/ticket_test.js
+++ ccem/assets/resources/js/ticket_test.js
@@ -1,26 +1,15 @@
 var topBarClient;
 var navBarClient;
 
-$(function(){
-	
-	client.get('instances').then(function(instancesData) {
-		var instances = instancesData.instances;
-		for ( var instanceGuid in instances) {
-			if (instances[instanceGuid].location === 'top_bar') {
-				topBarClient =  client.instance(instanceGuid);
-				topBarClient.trigger("getSidebarClient", client._instanceGuid);
-			}
+client.get('instances').then(function (instancesData) {
+	var instances = instancesData.instances;
+	for (var instanceGuid in instances) {
+		if (instances[instanceGuid].location === 'top_bar') {
+			topBarClient = client.instance(instanceGuid);
+			topBarClient.trigger("getSidebarClient", client._instanceGuid);
+		} else if (instances[instanceGuid].location === 'nav_bar') {
+			navBarClient = client.instance(instanceGuid);
+			navBarClient.trigger("getSidebarClient", client._instanceGuid);
 		}
-	});
-	
-	client.get('instances').then(function(instancesData) {
-		var instances = instancesData.instances;
-		for ( var instanceGuid in instances) {
-			if (instances[instanceGuid].location === 'nav_bar') {
-				navBarClient = client.instance(instanceGuid);
-				navBarClient.trigger("getSidebarClient", client._instanceGuid);
-			}
-		}
-	});
-
+	}
 });
