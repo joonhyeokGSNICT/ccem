9d219e56e3bd2e52e8fc3b615c88ecde4675df72
chiwonoh <gbajs2018@gmail.com>
2021-04-08 16:18:15 +0900

    티켓양식 변경 이벤트 추가

--- ccem/assets/resources/js/ticket_sidebar.js
+++ ccem/assets/resources/js/ticket_sidebar.js
@@ -1,13 +1,13 @@
 var topBarClient;
 var navBarClient;
 
-client.on('app.activated', function(){
-	topBarClient.trigger("getSidebarClient", client._instanceGuid);
-});
+const init = async function () {
 
-client.get('instances').then(function (instancesData) {
-	var instances = instancesData.instances;
-	for (var instanceGuid in instances) {
+	// get instances
+	const { instances } = await client.get('instances');
+
+	// send sidebar client
+	for (const instanceGuid in instances) {
 		if (instances[instanceGuid].location === 'top_bar') {
 			topBarClient = client.instance(instanceGuid);
 			topBarClient.trigger("getSidebarClient", client._instanceGuid);
@@ -16,8 +16,37 @@ client.get('instances').then(function (instancesData) {
 			navBarClient.trigger("getSidebarClient", client._instanceGuid);
 		}
 	}
+
+	ticketFieldDisable();
+
+}
+
+/**
+ * 티켓필드 비활성화
+ */
+const ticketFieldDisable = function () {
+
+	// 비활성화 티켓필드 목록
+	const field_ids = [
+		ZDK_INFO[_SPACE]["ticketField"]["CSEL_DATE_NO_SEQ"],	// 상담번호
+	];
+
+	// 비활성화
+	field_ids.forEach(el => client.invoke(`ticketFields:custom_field_${el}.disable`));
+	
+}
+
+/**
+ * 티켓사이드바 활성화 이벤트
+ */
+client.on('app.activated', function () {
+	topBarClient.trigger("getSidebarClient", client._instanceGuid);
 });
 
-// 티켓필드 disabled
-client.invoke(`ticketFields:custom_field_${ZDK_INFO[_SPACE]["ticketField"]["CSEL_DATE_NO_SEQ"]}.disable`);
+/**
+ * 티켓양식 변경 이벤트
+ */
+client.on("ticket.form.id.changed", ticketFieldDisable);
 
+
+init();
