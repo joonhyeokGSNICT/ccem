2a73705d6e46cadcd45c62b73a1b6139bb27da0f
chiwonoh <gbajs2018@gmail.com>
2021-04-27 17:47:43 +0900

    [상담등록] Zen저장버튼 추가

--- ccem/assets/resources/js/pop_CCEMPRO032.js
+++ ccem/assets/resources/js/pop_CCEMPRO032.js
@@ -72,8 +72,7 @@ const setEvent = () => {
 		ev.target.disabled = true;
 		const loading = new Loading(getLoadingSet('상담등록중 입니다.'));
 		
-		// onSave()
-		onSave("ZEN")
+		onSave()
 			.catch((error) => {
 				console.error(error);
 				const errMsg = error.responseText || error;
